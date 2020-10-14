5710b9abe2b93aa15439666ef9c276d1f1c2ff1f
James <jamesdennisgc@gmail.com>
2020-10-14 20:07:08 +1000

    Move code into recursive function as a crude implementation of change detection and clean up code. Add todos.

--- warn.js
+++ warn.js
@@ -6,21 +6,27 @@
  *
  * The following extension is intended for James Dennis only. it'sfreerealestate.
  *
- * TODO - needs logic re-think, will not detect changes to page, cancelling a post clears styles.
- * TODO - implement change detection on public/private status of post, clear styles on private posts.
+ * TODO - detect public/private status of post, clear styles on private posts
+ * TODO - implement change detection rather than looped timer
+ * TODO - update btn styles
+ * TODO - add icon
+ * TODO - build extension - build and src folder
  */
 
 console.log("🦍 warnscript active 🦍");
 
-const warnColour = "#fff8f8";
+const loopTime = 3000;
 const newPlaceholder = "🤠👆 Create new post...";
 const overrideElementClasses = "markdown_markdown-area__3s11P focus-target markdown-textarea sections_resizable__3HLQS sections_text-content__16ZYh";
+const warnColour = "#fff8f8";
 
 // Match full post input wrappers
 // const messageWrappers = document.getElementsByClassName("message-wrapper");
 
-// Arbitrary timeout to avoid load time issues
-setTimeout(function(){
+/**
+ * Recursive function to monitor page, match and override input attributes
+ */
+const monitorPage = () => {
     // Match Mavenlink input fields
     const postFields = document.getElementsByClassName(overrideElementClasses);
 
@@ -31,4 +37,10 @@ setTimeout(function(){
             postField.setAttribute('placeholder', newPlaceholder);
         }
     }
-}, 1000);
+
+    setTimeout(() => {
+        monitorPage();
+    }, loopTime);
+};
+
+monitorPage();
